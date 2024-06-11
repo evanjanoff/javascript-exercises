e742a8967c134c1362c155a0226279285e58e91f
Evan Janoff <evan.janoff@gmail.com>
2024-06-11 11:44:05 -0700

    Update with my terrible solution to problem 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+  let tempArray = [];
+  let reversedString = "";
+  for (let char of inputString) {
+    tempArray.push(char);
+  }
+  for (let i = tempArray.length - 1; i >= 0; i--) {
+    reversedString += tempArray[i];
+  }
+  return reversedString;
 };
 
 // Do not edit below this line
