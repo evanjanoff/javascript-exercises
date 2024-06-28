a506153a5ec1620462a75174b0ce33bb830b3c97
Evan Janoff <evan.janoff@gmail.com>
2024-06-28 15:40:39 -0700

    Solved Problem 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+  let inputString = string.toLowerCase();
+  let strippedString = [];
+  for (let i = 0; i < inputString.length; i++) {
+    const char = inputString.charAt(i);
+    if (char >= "a" && char <= "z" ||  char >= "0" && char <= "9")
+      strippedString.push(char);
+  }
+  for (i = 0; i < strippedString.length; i++){
+    if (strippedString[i] != strippedString[strippedString.length - 1 - i])
+      return false;
+    else 
+      continue;
+  }
+  return true;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
