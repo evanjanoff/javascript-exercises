6e81d1f9e5903c7b81e226465e92e9753fdb3029
Evan Janoff <evan.janoff@gmail.com>
2024-06-28 09:58:37 -0700

    Soultion to problem 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(x, y) {
+	let z = x + y;
+  return z;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	let z = x - y;
+  return z;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let z = 0;
+  array.forEach((number) => z += number);
+  return z;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let z = 1;
+  array.forEach((number) => z *= number);
+  return z;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	let z = x ** y;
+  return z;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let y = 1;
+	if (x > 1) {
+    for (let i = y; i <= x; i++)
+      y *= i;
+  }
+  return y;
 };
 
 // Do not edit below this line
