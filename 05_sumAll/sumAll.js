51ab18bb225d654e81aba50ec07b51b29197e6a7
Evan Janoff <evan.janoff@gmail.com>
2024-06-28 08:30:18 -0700

    Solution to problem 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(arg1, arg2) {
+  if (!Number.isInteger(arg1) || !Number.isInteger(arg2)) {
+    return "ERROR";
+  }
+  else if (arg1 < 1 || arg2 < 1) {
+    return "ERROR";
+  }
+  else {
+    if (arg1 < arg2) {
+      let sum = 0;
+      for (let i = arg1; i <= arg2; i++) {
+        sum += i;
+      }
+      return sum;
+    }
+    else {
+      let sum = 0;
+      for (let i = arg1; i >= arg2; i--) {
+        sum += i;
+      }
+      return sum;
+    }
+  }
 };
 
 // Do not edit below this line
