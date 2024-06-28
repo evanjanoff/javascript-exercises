bf2fdd841f77a48238606092dfd243b41a14333e
Evan Janoff <evan.janoff@gmail.com>
2024-06-28 09:35:31 -0700

    Solution to problem 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  let y = 5 / 9 * (x - 32);
+  return parseFloat(y.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+  let y = 9 / 5 * x + 32;
+  return parseFloat(y.toFixed(1));
 };
 
 // Do not edit below this line
