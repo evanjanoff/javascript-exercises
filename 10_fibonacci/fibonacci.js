e3cd48e52a7e41973a9e5fef826000cc97ee799c
Evan Janoff <evan.janoff@gmail.com>
2024-07-08 07:44:24 -0700

    Solution for prpblem 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibonacciNum) {
+  let x = Number(fibonacciNum);
+  if (x < 0)
+    return "OOPS";
+  else if (x == 0)
+    return 0;
+  else if (x == 1 || x == 2)
+    return 1;
+  else {
+    let y = 1;
+    let z = 0; 
+    for (let i = 2; i <= x; i++) {
+      let temp = y + z;
+      z = y;
+      y = temp;
+    }
+    return y;
+  }
 };
 
 // Do not edit below this line
